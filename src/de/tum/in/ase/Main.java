e59f0885a5bcc6e483969178cdb95b3940637d59
Mariya Koeva <mariya.koeva@tum.de>
2022-11-04 10:56:18 +0100

    Exercise-Template pushed by Artemis

---
+++ src/de/tum/in/ase/Main.java
@@ -0,0 +1,33 @@
+package de.tum.in.ase;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    //TODO create classes Participant, Lecturer, ExerciseInstructor, Tutor, Student according to UML diagrams (Hint: inheritance)
+    //TODO create classes Lecture and LectureHall
+    //TODO implement toString() methods
+    //TODO implement methods addParticipant and addParticipants in Lecture
+    //TODO implement openLecture() and closeLecture() in Lecturer
+    //TODO implement methods in LectureHall
+    //TODO implement method takePlace() in Lecture
+
+    public static void main(String[] args){
+        //TODO test your code here
+    }
+
+
+    //TODO implement methods generateStudents and generateTutors
+    //TODO think of the most suitable access modifier for those helper methods
+    /*
+    static List<Student> generateStudents(int amount){
+        return null;
+    }
+
+    static List<Tutor> generateTutors(int amount,Lecture lecture){
+        return null;
+    }
+
+     */
+}
